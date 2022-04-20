74b60d335fd6e83801f0070dd52d2e5580bf4c04
贾天龙 <604872657@qq.com>
2022-04-20 23:30:33 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/StudyCPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StudyCPP.dir/Day1/T1.cpp.obj"
+  "StudyCPP.exe"
+  "StudyCPP.exe.manifest"
+  "StudyCPP.lib"
+  "StudyCPP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StudyCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
